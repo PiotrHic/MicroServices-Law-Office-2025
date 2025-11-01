41378ce677fdea94d91007a0048a224b3ed9f2dd
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-01 19:49:54 +0100

    Create TDD for Lawyer Class (Domain). Closes #8.

---
+++ Lawyer-Service/src/test/java/org/example/lawyerservice/domain/LawyerTest.java
@@ -0,0 +1,10 @@
+package org.example.lawyerservice.domain;
+
+public class LawyerTest {
+    public void setId() {}
+    public void getId(){}
+    public void setName() {}
+    public void getName() {}
+    public void setLawCases(){}
+    public void getLawCases(){}
+}
