2c83625c4b41ed43d7f5e3078ae5ab19a43df0fc
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-01 18:06:54 +0100

    Create Lawyer-Service Module. Closes #4.

---
+++ Lawyer-Service/src/main/java/org/example/lawyerservice/LawyerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.lawyerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawyerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawyerServiceApplication.class, args);
+    }
+
+}
