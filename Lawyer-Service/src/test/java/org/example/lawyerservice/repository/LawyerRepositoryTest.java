a393761b3635ae7736e01f3effd6acbd362e0bd1
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-01 19:33:17 +0100

    Create TDD for Lawyer Repository. Closes #6

---
+++ Lawyer-Service/src/test/java/org/example/lawyerservice/repository/LawyerRepositoryTest.java
@@ -0,0 +1,16 @@
+package org.example.lawyerservice.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class LawyerRepositoryTest {
+
+    void testsSetUp(){}
+    void createLawyer(){}
+    void updateLawyer(){}
+    void getAllLawyers(){}
+    void getOneLawyerById(){}
+    void deleteLawyerById(){}
+    void deleteAllLawyersId(){}
+}
