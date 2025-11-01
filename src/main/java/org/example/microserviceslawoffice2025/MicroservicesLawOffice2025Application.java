22fb1442d13498cc6d38cdfe95402fcb41937d15
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-01 17:28:39 +0100

    Initial Project Add. Closes #1

---
+++ src/main/java/org/example/microserviceslawoffice2025/MicroservicesLawOffice2025Application.java
@@ -0,0 +1,13 @@
+package org.example.microserviceslawoffice2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesLawOffice2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesLawOffice2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/microserviceslawoffice2025/MicroservicesLawOffice2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.microserviceslawoffice2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesLawOffice2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
