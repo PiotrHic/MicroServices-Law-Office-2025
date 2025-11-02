2dfbdeeae99bc34c51aee963ca87d4f3b68b7034
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-02 10:11:35 +0100

    Create TDD for Lawyer Service. Closes #15.

---
+++ Lawyer-Service/src/test/java/org/example/lawyerservice/service/LawyerServiceTest.java
@@ -0,0 +1,16 @@
+package org.example.lawyerservice.service;
+
+import org.example.lawyerservice.domain.Lawyer;
+
+public class LawyerServiceTest {
+
+    void addLawyerTest(Lawyer lawyer){};
+    void getLawyerByIDTest(String id){};
+    void getLawyerByNaneTest(String name){};
+    void getAllLawyersTest(){};
+    void updateLawyerByIdTest(String id, Lawyer lawyer){};
+    void updateLawyerByNameTest(String name, Lawyer lawyer){};
+    void deleteByIdTest(String id){};
+    void deleteLawyerByNameTest(String name){};
+    void deleteAllTest(){};
+}
