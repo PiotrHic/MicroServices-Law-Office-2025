aab15b752ed89d9489809defd34611692cfe3c0e
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-03 21:43:58 +0100

    Implement R - Read methods in Controller and test them. Closes #35.

--- Lawyer-Service/src/main/java/org/example/lawyerservice/controller/LawyerController.java
+++ Lawyer-Service/src/main/java/org/example/lawyerservice/controller/LawyerController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
@@ -27,17 +28,21 @@ public class LawyerController {
     private static final Logger LOGGER
             = LoggerFactory.getLogger(LawyerController.class);
 
-    private final String NUMBER_PATH = "{lawyerId}";
-    private final String PATH_VARIABLE_PATH = "lawyerId";
+    private final String NUMBER_PATH = "/findById/{lawyerId}";
+    private final String NUMBER_VARIABLE_PATH = "lawyerId";
+
+    private final String NAME_PATH = "/fingByName/{lawyerName}";
+    private final String NAME_VARIABLE_PATH = "lawyerName";
 
     @GetMapping("/testMethod")
     ResponseEntity<LawyerDTO> testMethod() {
-        Lawyer testLawyer = Lawyer.builder().id("1").name("testMethod").build();
+        Lawyer testLawyer = Lawyer.builder().id("1").name("test_Method").build();
         LawyerDTO responseDTO = modelMapper.map(testLawyer, LawyerDTO.class);
         ResponseEntity<LawyerDTO> response = new ResponseEntity<>(responseDTO, HttpStatusCode.valueOf(201));
         LOGGER.info("Test Method was requested!");
         return response;
     }
+
     @PostMapping
     ResponseEntity<LawyerDTO> createLawyer(@RequestBody LawyerDTO lawyerDTO){
         Lawyer added = lawyerService.addLawyer(modelMapper.map(lawyerDTO,Lawyer.class));
@@ -45,15 +50,34 @@ public class LawyerController {
         return new ResponseEntity<>(modelMapper.map(added,LawyerDTO.class),
                 HttpStatus.valueOf(201));
     }
-    Lawyer getLawyerByID(String id) {
-        return null;
+
+    @GetMapping(NUMBER_PATH)
+    ResponseEntity<LawyerDTO> getLawyerById(@PathVariable(NUMBER_VARIABLE_PATH) String lawyerId) {
+        LawyerDTO foundedById = modelMapper.map(lawyerService.getLawyerByID(lawyerId),LawyerDTO.class);
+        LOGGER.info("Lawyer: {} was founded by id in the database!", foundedById.getName());
+        return new ResponseEntity<>(foundedById, HttpStatus.valueOf(200));
     }
-    Lawyer getLawyerByName(String name) {
-        return null;
+
+    @GetMapping(NAME_PATH)
+    ResponseEntity<LawyerDTO> getLawyerByName(@PathVariable(NAME_VARIABLE_PATH) String lawyerName) {
+        LawyerDTO foundedByName = modelMapper.map(lawyerService.getLawyerByName(lawyerName),LawyerDTO.class);
+        LOGGER.info("Lawyer: {} was founded by name in the database!", foundedByName.getName());
+        return new ResponseEntity<>(foundedByName, HttpStatus.valueOf(200));
     }
-    List<Lawyer> getAllLawyers() {
-        return null;
+
+    @GetMapping("/getAllLawyers")
+    ResponseEntity<List<LawyerDTO>> getAllLawyers() {
+        List<Lawyer> lawyers = lawyerService.getAllLawyers();
+        if (lawyers.isEmpty()) {
+            throw new LayerInstantiationException("There is no lawyers in the database!");
+        }
+        LOGGER.info("All Lawyers were founded!");
+        List<LawyerDTO> lawyersDTO = lawyers.stream()
+                .map(lawyer -> modelMapper.map(lawyer,LawyerDTO.class))
+                .toList();
+        return new ResponseEntity<>(lawyersDTO, HttpStatus.valueOf(200));
     }
+
     Lawyer updateLawyerById(String id, Lawyer lawyer) {
         return null;
     }
@@ -66,7 +90,7 @@ public class LawyerController {
     Lawyer deleteLawyerByName(String name){
         return null;
     }
-    String deleteAlLawyers(){
+    ResponseEntity <String> deleteAlLawyers(){
         return null;
     }
 }
