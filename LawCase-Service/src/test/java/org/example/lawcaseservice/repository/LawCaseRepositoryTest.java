62537c9825043684c02344784dfa632dcac99b16
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-07 18:50:16 +0100

    Create TDD for LawCase Repository.Closes 50.

---
+++ LawCase-Service/src/test/java/org/example/lawcaseservice/repository/LawCaseRepositoryTest.java
@@ -0,0 +1,42 @@
+package org.example.lawcaseservice.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class LawCaseRepositoryTest {
+
+    @BeforeEach
+    void setUp(){
+
+    }
+
+
+    @Test
+    void createLawCase(){
+    }
+    @Test
+    void updateLawCase(){
+    }
+
+
+
+    @Test
+    void getAllLawCases(){
+    }
+
+    @Test
+    void getOneLawCaseById(){
+
+    }
+
+    @Test
+    void deleteLawCaseById(){
+
+    }
+
+
+    @Test
+    void deleteAllLawCases() {
+    }
+}
