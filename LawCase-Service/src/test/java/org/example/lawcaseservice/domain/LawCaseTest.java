5183696a3462cdd1d5ee09f473a06bcab8db05a6
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-07 18:35:21 +0100

    Create TDD for LawCase Class. Closes #45.

---
+++ LawCase-Service/src/test/java/org/example/lawcaseservice/domain/LawCaseTest.java
@@ -0,0 +1,56 @@
+package org.example.lawcaseservice.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class LawCaseTest {
+
+    @Test
+    public void setId(){
+    }
+
+    @Test
+    public void getId(){
+    }
+
+    @Test
+    public void setName(){
+    }
+
+    @Test
+    public void getName(){
+    }
+
+    @Test
+    public void setLawyerId(){
+    }
+
+    @Test
+    public void getLawyerId(){
+    }
+
+    @Test
+    public void setLawClientId(){
+    }
+
+    @Test
+    public void getLawClientId(){
+    }
+
+    @Test
+    public void setLawyer(){
+    }
+
+    @Test
+    public void getLawyer(){
+    }
+
+    @Test
+    public void setLawClient(){
+    }
+
+    @Test
+    public void getLawClient(){
+    }
+
+
+}
