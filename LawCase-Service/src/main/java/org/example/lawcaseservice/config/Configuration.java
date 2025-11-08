902685f620962504291f73109464035328343191
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-08 13:51:01 +0100

    Create <apper for the LawCase Service. Closes #62.

---
+++ LawCase-Service/src/main/java/org/example/lawcaseservice/config/Configuration.java
@@ -0,0 +1,13 @@
+package org.example.lawcaseservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
