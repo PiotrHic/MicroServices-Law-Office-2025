f8cb3d95712eed42659ebed9b0afc9f606aef848
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-08 13:38:08 +0100

    Create TDD for the LawCase Controller. Closes #61.

---
+++ LawCase-Service/src/test/java/org/example/lawcaseservice/controller/LawCaseControllerTest.java
@@ -0,0 +1,51 @@
+package org.example.lawcaseservice.controller;
+
+import org.example.lawyerservice.domain.Lawyer;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class LawCaseControllerTest {
+
+    @Test
+    void testCreateLawCase() {
+    }
+
+    @Test
+    void testGetLawCaseById() {
+    }
+
+    @Test
+    void testGetLawCaseByName() {
+    }
+
+    @Test
+    void testGetAllLawCase() {
+    }
+
+    @Test
+    void testUpdateLawCaseById() {
+    }
+
+    @Test
+    void testUpdateLawCaserByName() {
+    }
+
+    @Test
+    void testDeleteLawCaserById() {
+
+    }
+
+    @Test
+    void testDeleteLawCaseByName() {
+    }
+
+    @Test
+    void testDeleteAllLawCases() {
+
+    }
+
+    @Test
+    void testGetLawCaserById_NotFound() {
+    }
+}
