9b8fb27f3df2fc32b031a8b7284d8fca4753fbee
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-12 19:12:06 +0100

    Create LawClient class in domain package. Closes #78.

--- LawClient-Service/src/main/java/org/example/lawclientservice/domain/LawClient.java
+++ LawClient-Service/src/main/java/org/example/lawclientservice/domain/LawClient.java
@@ -1,10 +1,11 @@
-package org.example.lawyerservice.domain;
+package org.example.lawclientservice.domain;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.List;
 
 @Getter
@@ -13,18 +14,13 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 @EqualsAndHashCode
-@Document(collection = "Lawyers")
-public class Lawyer {
+@Document(collection = "LawClients")
+public class LawClient {
 
-    public Lawyer(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
     @Id
     private String id;
     @NotBlank(message = "Name is required!")
     @Size(min=4, message = "Name must have at least 4 characters!")
     private String name;
-    private int yearsOfExperience;
-    private List<LawCase> lawCaseList; // for later
+    private List<LawCase> lawCaseList;
 }
