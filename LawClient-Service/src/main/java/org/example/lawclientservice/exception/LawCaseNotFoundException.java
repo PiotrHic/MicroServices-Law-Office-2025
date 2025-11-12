bc0e7c598711850dfc097327c7cef904af19a967
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-12 21:37:27 +0100

    Create LawCaseNotFoundException. Closes #87.

---
+++ LawClient-Service/src/main/java/org/example/lawclientservice/exception/LawCaseNotFoundException.java
@@ -0,0 +1,12 @@
+package org.example.lawclientservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No such LawCase in Repository")
+public class LawCaseNotFoundException extends RuntimeException {
+
+    public LawCaseNotFoundException(String message) {
+        super(message);
+    }
+}
