642d260371a7f6d2e65d3cdd8fc229765efd531e
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-12 21:12:38 +0100

    Create Methods for Service Layer Interface. Closes #89.

---
+++ LawClient-Service/src/main/java/org/example/lawclientservice/service/LawClientService.java
@@ -0,0 +1,26 @@
+package org.example.lawclientservice.service;
+
+import org.example.lawclientservice.domain.LawClient;
+
+import java.util.List;
+
+public interface LawClientService {
+
+    // C
+    LawClient createLawClient(LawClient lawClient);
+
+    // R
+    LawClient getLawClientByID (String lawClientId);
+    LawClient getLawClientByName (String name);
+    List<LawClient> getAllLawClients();
+
+    // U
+    LawClient updateLawClientById(String lawClientId, LawClient lawClient);
+    LawClient updateLawClientByName(String name, LawClient lawClient);
+
+    // D
+    String deleteLawClientById(String lawClientId);
+    String deleteLawClientByName(String name);
+    String deleteAllLawClients();
+
+}
