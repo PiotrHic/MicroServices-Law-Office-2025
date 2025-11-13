8680ef9fab185419711a2df7cbef17219ce54751
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-13 15:37:26 +0100

    Create WebClient config for LawCase Service to get data from Lawyer Service. Closes #107

---
+++ LawCase-Service/src/main/java/org/example/lawcaseservice/config/WebClientLawyerConfig.java
@@ -0,0 +1,35 @@
+package org.example.lawcaseservice.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancedExchangeFilterFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class WebClientLawyerConfig {
+
+    @Autowired
+    private LoadBalancedExchangeFilterFunction filterFunction;
+
+    @Bean
+    public WebClient lawyerWebClient() {
+        return WebClient.builder()
+                .baseUrl("http://Lawyer-Service")
+                .filter(filterFunction)
+                .build();
+    }
+
+    @Bean
+    public LawyerClient lawyerClient() {
+        HttpServiceProxyFactory httpServiceProxyFactory =
+                HttpServiceProxyFactory
+                        .builderFor(WebClientAdapter.create(lawyerWebClient()))
+                        .build();
+        return httpServiceProxyFactory.createClient(LawyerClient.class);
+    }
+
+}
