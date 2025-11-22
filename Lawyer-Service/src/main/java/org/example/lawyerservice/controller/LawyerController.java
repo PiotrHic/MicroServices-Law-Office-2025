f3ef52e0136d2f91da7ac3d8a4818d04132869c0
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-11-22 22:08:39 +0100

    Improve and enchance Api Documentation for the Lawyer Service. Closes #127.

--- Lawyer-Service/src/main/java/org/example/lawyerservice/controller/LawyerController.java
+++ Lawyer-Service/src/main/java/org/example/lawyerservice/controller/LawyerController.java
@@ -3,6 +3,7 @@ package org.example.lawyerservice.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.example.lawyerservice.client.LawCaseClient;
@@ -20,6 +21,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/lawyer")
+@Tag(name = "Lawyer Controller")
 public class LawyerController {
 
     private final LawyerService lawyerService;
@@ -35,12 +37,11 @@ public class LawyerController {
 
 
     @Operation(
-            summary = "Add new Lawyer",
-            description = "Creates a new lawyer and stores it in the database."
+            description = "Creates a new lawyer and stores it in the database"
     )
     @ApiResponses({
             @ApiResponse(responseCode = "201", description = "Lawyer created successfully"),
-            @ApiResponse(responseCode = "400", description = "Invalid input data")
+            @ApiResponse(responseCode = "500", description = "Invalid input data")
     })
     @PostMapping
     ResponseEntity<LawyerDTO> createLawyer(@Valid @RequestBody LawyerDTO lawyerDTO){
@@ -50,6 +51,13 @@ public class LawyerController {
                 HttpStatus.valueOf(201));
     }
 
+    @Operation(
+            description = "Get Lawyer from the database by the id  - api/lawyer/getById/1"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lawyer delivered successfully"),
+            @ApiResponse(responseCode = "404", description = "Lawyer was not found")
+    })
     @GetMapping("/getById/{lawyerId}")
     ResponseEntity<LawyerDTO> getLawyerById(@PathVariable(NUMBER_VARIABLE_PATH) String lawyerId) {
         LawyerDTO foundedById = modelMapper.map(lawyerService.getLawyerByID(lawyerId),LawyerDTO.class);
@@ -57,6 +65,13 @@ public class LawyerController {
         return new ResponseEntity<>(foundedById, HttpStatus.valueOf(200));
     }
 
+    @Operation(
+            description = "Get Lawyer from the database by the name  - api/lawyer/getByName?lawyerName=Piotr+Hic"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lawyer delivered successfully"),
+            @ApiResponse(responseCode = "404", description = "Lawyer was not found")
+    })
     @GetMapping("/getByName") // ?lawyerName=
     ResponseEntity<LawyerDTO> getLawyerByName(@RequestParam(NAME_VARIABLE_PATH) String lawyerName) {
         LawyerDTO foundedByName = modelMapper.map(lawyerService.getLawyerByName(lawyerName),LawyerDTO.class);
@@ -64,6 +79,13 @@ public class LawyerController {
         return new ResponseEntity<>(foundedByName, HttpStatus.valueOf(200));
     }
 
+    @Operation(
+            description = "Get all Lawyers from the Database"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lawyers delivered"),
+            @ApiResponse(responseCode = "500", description = "Empty Database")
+    })
     @GetMapping("/getAllLawyers")
     ResponseEntity<List<LawyerDTO>> getAllLawyers() {
         List<Lawyer> lawyers = lawyerService.getAllLawyers();
@@ -77,6 +99,14 @@ public class LawyerController {
         return new ResponseEntity<>(lawyersDTO, HttpStatus.valueOf(200));
     }
 
+    @Operation(
+            description = "Update Lawyer from the database by the id  - /api/lawyer/updateById/1"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lawyer delivered successfully"),
+            @ApiResponse(responseCode = "404", description = "Lawyer was not found by id"),
+            @ApiResponse(responseCode = "500", description = "Parameter was not correct"),
+    })
     @PutMapping("/updateById/{lawyerId}")
     ResponseEntity<LawyerDTO> updateLawyerById(@PathVariable(NUMBER_VARIABLE_PATH) String lawyerId,
                                                @Valid @RequestBody LawyerDTO lawyerDTO) {
@@ -87,6 +117,14 @@ public class LawyerController {
         return new ResponseEntity<>(updatedDTO, HttpStatus.valueOf(200));
     }
 
+    @Operation(
+            description = "Update Lawyer from the database by the name  - /api/lawyer/updateByName?lawyerName=Piotr+Hic"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lawyer delivered successfully"),
+            @ApiResponse(responseCode = "404", description = "Lawyer was not found by name"),
+            @ApiResponse(responseCode = "500", description = "Parameter was not correct"),
+    })
     @PutMapping("/updateByName") // ?lawyerName=
     ResponseEntity<LawyerDTO> updateLawyerByName(@RequestParam String lawyerName,@Valid @RequestBody LawyerDTO lawyerDTO){
         Lawyer toUpdate = modelMapper.map(lawyerDTO, Lawyer.class);
@@ -96,6 +134,13 @@ public class LawyerController {
         return new ResponseEntity<>(updatedDTO, HttpStatus.valueOf(200));
     }
 
+    @Operation(
+            description = "Delete Lawyer from the database by the id  - /api/lawyer/deleteById/1"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lawyer deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Lawyer was not found by id")
+    })
     @DeleteMapping("/deleteById/{lawyerId}")
     ResponseEntity <LawyerDTO> deleteLawyerById(@PathVariable(NUMBER_VARIABLE_PATH) String lawyerId){
         LawyerDTO deleted = modelMapper.map(lawyerService.deleteLawyerById(lawyerId), LawyerDTO.class);
@@ -103,6 +148,13 @@ public class LawyerController {
         return new ResponseEntity<>(deleted, HttpStatus.OK);
     }
 
+    @Operation(
+            description = "Delete Lawyer from the database by the name  - deleteByName?lawyerName=Piotr+Hic"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lawyer deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Lawyer was not found by name")
+    })
     @DeleteMapping("/deleteByName") // ?lawyerName=
     ResponseEntity <LawyerDTO> deleteLawyerByName(@RequestParam String lawyerName){
         Lawyer deleted= lawyerService.deleteLawyerByName(lawyerName);
@@ -111,6 +163,12 @@ public class LawyerController {
         return new ResponseEntity<>(updatedDTO, HttpStatus.valueOf(200));
     }
 
+    @Operation(
+            description = "Delete all Lawyers from the Database"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "All Lawyers deleted")
+    })
     @DeleteMapping("/deleteAll")
     ResponseEntity <String> deleteAlLawyers(){
         lawyerService.deleteAlLawyers();
@@ -121,11 +179,27 @@ public class LawyerController {
     // WebClient methods
 
     // LawCase
+
+    @Operation(
+            description = "Send Lawyer to the LawCase microservice"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lawyer delivered by Id and attached to the LawCase"),
+            @ApiResponse(responseCode = "500", description = "Some internal server error")
+    })
     @GetMapping("/toBringLawyer/{lawyerId}")
-    public Lawyer findLawyerByLawyerId(@PathVariable(NAME_VARIABLE_PATH) String lawyerId){
+    public Lawyer findLawyerByLawyerId(@PathVariable("lawyerId") String lawyerId){
         return lawyerService.getLawyerByID(lawyerId);
     }
 
+    @Operation(
+            description = "Get List of LawCases by Lawyer Id"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "LawCases delivered by Id and attached to the LawCase"),
+            @ApiResponse(responseCode = "404", description = "Lawyer was not found"),
+            @ApiResponse(responseCode = "500", description = "Some internal server error")
+    })
     @GetMapping("/forLawCases/{lawyerId}")
     public Lawyer findLawCaseByLawyerId(@PathVariable("lawyerId") String lawyerId){
         Lawyer founded = lawyerService.getLawyerByID(lawyerId);
@@ -133,6 +207,14 @@ public class LawyerController {
         return founded;
     }
 
+    @Operation(
+            description = "Get List of LawCases by Lawyer Id"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "LawCases delivered by Id and attached to the LawCase"),
+            @ApiResponse(responseCode = "404", description = "Lawyer was not found"),
+            @ApiResponse(responseCode = "500", description = "Some internal server error")
+    })
     @GetMapping("/forLawCases-withLawClient/{lawyerId}")
     public Lawyer findLawCaseWithLawClientsByLawyerId(@PathVariable("lawyerId") String lawyerId){
         Lawyer founded = lawyerService.getLawyerByID(lawyerId);
