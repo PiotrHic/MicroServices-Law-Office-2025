6ec0428fec165d0dbdb7de865d80f8754171c9f9
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-12-10 19:17:25 +0100

    Create frontend-vite branch

--- frontend/src/api/clientsApi.js
+++
@@ -1,7 +0,0 @@
-import api from "./axiosClient";
-
-export const getClients = () => api.get("/clients");
-export const getClient = (id) => api.get(`/clients/${id}`);
-export const createClient = (data) => api.post("/clients", data);
-export const updateClient = (id, data) => api.put(`/clients/${id}`, data);
-export const deleteClient = (id) => api.delete(`/clients/${id}`);
